046bd30297976dbc5f06fe435e9dcba771b038c5
manik-setia <setia.manik001@gmail.com>
2022-08-13 06:52:41 +0530

    Added required dependencies and datasource properties

---
+++ src/main/java/com/test/permissionbasedauthorization/PermissionBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.test.permissionbasedauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionBasedAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionBasedAuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/permissionbasedauthorization/PermissionBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.permissionbasedauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionBasedAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
